ed084ed0094c69e3c2ec91a97e5e5eda7ac0842b
evelynlizzethe <elriofrio1@utpl.edu.ec>
2023-12-07 16:58:19 -0500

    Taller08

--- problematicas/Problema01/src/problema01/Problema01.java
+++ problematicas/Problema01/src/problema01/Problema01.java
@@ -4,6 +4,9 @@
  */
 package problema01;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 /**
  *
  * @author LAB.ELECT
@@ -15,6 +18,55 @@ public class Problema01 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+       Scanner entrada = new Scanner(System.in);
+       entrada.useLocale(Locale.US);
+
+       String nombre;
+       String posicion;
+       int edad;
+       double estatura;
+       double promedioEstaturas = 0;
+       double promedioEdades = 0;
+       String mensajeFinal = "";
+       int jugador = 0;
+       int edades = 0; 
+       double estaturas = 0;
+       String sumaEdades = ""; 
+       
+       System.out.println("Ingrese la cantidad de jugadores");
+       jugador = entrada.nextInt();
+       
+       for (int i = 1; i <= jugador; i++){
+           entrada.nextLine();
+           System.out.println("Ingrese el nombre del jugador");
+           nombre = entrada.nextLine();
+           System.out.println("Ingrese la posicion del jugador");
+           posicion = entrada.nextLine();
+           System.out.println("Ingrese la estatura del jugador");
+           estatura = entrada.nextDouble();
+           System.out.println("Ingrese la edad del jugador");
+           edad = entrada.nextInt();
+           
+           sumaEdades = String.format("%s%d", sumaEdades,edad);
+           
+           mensajeFinal = String.format("%s%d.%s-%s-,edad %d, estatura %.2f\\n",
+                   mensajeFinal, i, nombre, posicion, edad,
+                   estatura);
+           
+           edades = edades + edad;
+           estaturas = estaturas + estatura;
+           
+           promedioEdades = edades / 1;
+           promedioEstaturas = estaturas / i;
+       }
+       
+       System.out.printf("Listado de Jugadores%s\nListado de Edades: %s\n"
+               + "Promedio de Edades: %.1f\nPromedio de Estaturas: %.2f\n"
+               ,mensajeFinal,
+               sumaEdades,
+               promedioEdades, 
+               promedioEstaturas);
+       
     }
     
 }
--- problematicas/Problema03/src/problema03/Problema03.java
+++ problematicas/Problema03/src/problema03/Problema03.java
@@ -15,6 +15,15 @@ public class Problema03 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+       double celsius;
+        String cadena = "";
+        for (int farenheit = 20; farenheit <= 96; farenheit += 4) {
+            celsius = (double) (farenheit - 32) * 5 / 9;
+            cadena= String.format("%s%d Farenheit son: %.2f Celsius\n",
+                    cadena, farenheit, celsius);
+        }
+        System.out.printf("%s", cadena);
+        
     }
     
 }
